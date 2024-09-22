2ce5df38fccf372730064a48af1ba5db3abce03a
Holly Ann Coloma <holly.life.15@gmail.com>
2024-09-22 17:08:11 +0000

    Add images and icons, style on headers. Update base template

--- doughndrizzle/settings.py
+++ doughndrizzle/settings.py
@@ -151,7 +151,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static and media files
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+# Absolute path to the directory where static files are stored
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Ensure this path points to your static folder
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
