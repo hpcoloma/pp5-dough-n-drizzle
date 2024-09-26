a7d179c8aed979cf69059b9fb05df1c8c2cec155
Holly Ann Coloma <holly.life.15@gmail.com>
2024-09-26 21:21:38 +0000

    Update fixtures, add media, refactor style for header, add product and category models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+# Category Admin Class
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'friendly_name')
+
+# Product Admin Class
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'description', 'image')
+    search_fields = ('name', 'category__name')
+    list_filter = ('category',)
+
+# Register the models with the admin site
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    friendly_name = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)  # Increased max_digits to allow for larger prices
+    image = models.ImageField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
