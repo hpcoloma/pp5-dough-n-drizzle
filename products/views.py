22ec049b51d81bf79ce1677fb595d2d1549193e4
Holly Ann Coloma <holly.life.15@gmail.com>
2024-09-27 21:20:32 +0000

    Started product template, add custom styling on product cards, update views to show categories, change currency to euro

--- products/views.py
+++ products/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
-    products = Product.objects.all()
+    categories = Category.objects.all()
+    products_by_category = {}
+
+    for category in categories:
+        products_by_category[category] = Product.objects.filter(category=category)
 
     context = {
-        'products': products,
+        'products_by_category': products_by_category,
     }
 
     return render(request, 'products/products.html', context)
