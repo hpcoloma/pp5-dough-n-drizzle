5f521f1f8c238fe798cb584ca23934be349ac4d7
Holly Ann Coloma <holly.life.15@gmail.com>
2024-10-06 23:00:29 +0000

    Add remove functionality on the cart template

--- cart/views.py
+++ cart/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, redirect
-# from products.model
+from django.shortcuts import render, reverse, redirect, HttpResponse, get_object_or_404
+from django.contrib import messages
 
-# Create your views here.
+from products.models import Product
+
 
 def view_cart(request):
     """A view that renders the bag contents page"""
     
     return render(request, 'cart/cart.html')
+
 
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
@@ -21,7 +23,41 @@ def add_to_cart(request, item_id):
         cart[item_id] = quantity
 
     request.session['cart'] = cart
-    print(request.POST)  # This will show what data is being submitted in the request
     return redirect(redirect_url)
 
 
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {cart[item_id]}')
+    else:
+        cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cart')
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        cart = request.session.get('cart', {})
+
+        cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cart')
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
