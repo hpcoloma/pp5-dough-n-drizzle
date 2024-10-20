8d1e5474d7b73f43740c51dddebadc29810bffe8
Holly Ann Coloma <holly.life.15@gmail.com>
2024-10-20 11:54:30 +0000

    Add snippets for code refactoring, update add product functionality

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('order_history/<order_number>', views.order_history, name='order_history'),
+    path('order_history/<str:order_number>/', views.order_history, name='order_history'),
     path('admin_management', views.admin_management, name='admin_management')
 ]
